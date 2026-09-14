e1f316d284c34d044b35671b66bbeeeb81dfbe28
Ezekiel Hicks <51956052+Zeekg4@users.noreply.github.com>
2026-09-14 15:39:14 -0500

    updates

--- study_tracker.py
+++ study_tracker.py
@@ -1,8 +1,12 @@
 student_name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 weekly_study_hours = float(input("What is your goal for weekly study hours? "))
-curent_study_hours = float(input("How many hours have you studied this week? "))
-
-study_hours_remaining = curent_study_hours / weekly_study_hours * 100
+current_study_hours = float(input("How many hours have you studied this week? "))
+remaining_study_hours = (weekly_study_hours - current_study_hours)
+remaining_study_percentage = (remaining_study_hours / weekly_study_hours) * 100
 print(f'\n STUDY TRACKER')
-print(f'You have {int(study_hours_remaining)}% of your weekly study goal remaining.')
+print(f'Name: {student_name}')
+print(f'Age: {age}')
+print(f'You have {int(remaining_study_hours)} hours of your weekly study goal remaining.')
+print(f'You have completed {int(current_study_hours)} hours of your weekly study goal.')
+print(f'You have {int(remaining_study_percentage)}% of your weekly study goal remaining.')
