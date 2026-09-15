f24316ce77ccdd77d28e21c172c5e6777ab59c8a
Ezekiel Hicks <51956052+Zeekg4@users.noreply.github.com>
2026-09-15 11:07:50 -0500

    added fix to remaining_hours decision making logic.

--- study_goal.py
+++ study_goal.py
@@ -2,7 +2,7 @@ name = str(input("Whats your name?"))
 weekly_study_goal = float(input("What is your weekly study goal in hours?"))
 hours_studied = float(input("How many hours have you studied this week?"))
 remaining_hours = int(weekly_study_goal - hours_studied)
-if remaining_hours < 0:
+if remaining_hours <= 0:
     print(f"Hello, {name}! You have reached your weekly study goal.")
 else:
     print(f"Hello, {name}! You have {remaining_hours} hours left to meet your weekly study goal.")
